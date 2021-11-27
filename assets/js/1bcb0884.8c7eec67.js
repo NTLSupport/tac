8846c010184c7c2494fa1fa481f5374351738349
Shoaib <zodpixel@gmail.com>
2021-11-27 17:50:07 +0500

    Deploy website - based on

--- assets/js/1bcb0884.8c7eec67.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[684],{1818:function(s){s.exports=JSON.parse('{"allTagsPath":"/tac/blog/tags","slug":"/tac/blog/tags/docusaurus","name":"docusaurus","count":4,"permalink":"/tac/blog/tags/docusaurus"}')}}]);
---
+++ assets/js/9238b0e2.09f00f8e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[76],{35:function(e){e.exports=JSON.parse('{"allTagsPath":"/tac/blog/tags","slug":"/tac/blog/tags/greetings","name":"greetings","count":1,"permalink":"/tac/blog/tags/greetings"}')}}]);
