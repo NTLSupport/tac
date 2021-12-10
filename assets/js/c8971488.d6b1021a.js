577e3b2ae72bced98997200b709b8ef7a7f857c2
Shoaib Sajid <zodpixel@gmail.com>
2021-12-10 19:11:21 +0500

    Deploy website - based on

--- assets/js/c8971488.d6b1021a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7511],{1557:function(e){e.exports=JSON.parse('{"permalink":"/tac/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":9,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
